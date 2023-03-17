75cd8a3e4fc7a4a4f67b0751eab341b3540a0b51
Gi5lund <mortengislund@gmail.com>
2023-03-17 14:04:16 +0100

    Initial commit

---
+++ src/main/java/dk/kea/webshopdemo/WebshopdemoApplication.java
@@ -0,0 +1,15 @@
+package dk.kea.webshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopdemoApplication
+	{
+
+		public static void main(String[] args)
+			{
+				SpringApplication.run(WebshopdemoApplication.class, args);
+			}
+
+	}
---
+++ src/test/java/dk/kea/webshopdemo/WebshopdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package dk.kea.webshopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopdemoApplicationTests
+	{
+
+		@Test
+		void contextLoads()
+			{
+			}
+
+	}
