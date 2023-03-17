f734875257f4e94622b3eacd4c493ca28751d789
Gi5lund <mortengislund@gmail.com>
2023-03-17 15:28:38 +0100

    created product class and repository

---
+++ src/main/java/dk/kea/webshopdemo/repository/ProductRepository.java
@@ -0,0 +1,40 @@
+package dk.kea.webshopdemo.repository;
+
+import dk.kea.webshopdemo.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class ProductRepository
+	{
+		// database propertie
+		private final static	String db_URL="jdbs:mysql://localhost:3306/webshopdemo";
+		private final String uid="root;";
+		private final String pwd="Pegasus2606";
+
+		public List<Product> getAll(){
+			List<Product> productList=new ArrayList<>();
+			try {
+				Connection connection= DriverManager.getConnection(db_URL,uid,pwd);
+				Statement statement=connection.createStatement();
+				final String SQL_QUERY="SELECT * FROM products";
+				ResultSet resultSet=statement.executeQuery(SQL_QUERY);
+				while (resultSet.next()){
+					int id=resultSet.getInt(1);
+					String name=resultSet.getString(2);
+					double price=resultSet.getDouble(3);
+					Product product=new Product(id,name,price);
+					productList.add(product);
+					System.out.println(product);
+				}
+			}catch (SQLException e){
+				System.out.println("could not query databse");
+				e.printStackTrace();
+			}
+			return productList;
+		}
+
+
+	}
